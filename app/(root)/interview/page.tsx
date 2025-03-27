3be91fb6fc28fd3cb77e238c0b176f2926541f02
toan trinh <solulink@gmail.com>
2025-03-27 17:55:34 -0500

    implement interview generation page AI

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,14 @@
+import Agent from '@/app/components/Agent'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <h3>Interview Generation</h3>
+
+      <Agent userName="you" userId="user1"></Agent>
+    </>
+  )
+}
+
+export default page
